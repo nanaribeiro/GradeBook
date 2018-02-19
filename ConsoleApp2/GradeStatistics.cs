d67bb570b9883f71298946995e6defcee85d0ec5
alAlana <alana.sasse@DES-0331>
2018-02-19 14:16:04 -0300

    Testing

---
+++ ConsoleApp2/GradeStatistics.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2
+{
+    class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+
+            GradeStatistics stats= book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.HighestGrade);
+        }
+    }
+
+}
